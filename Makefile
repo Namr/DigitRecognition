c95aebc321398531ac552073f1b983a339bbaf3c
Amr Moussa <amrmoussa100@gmail.com>
2018-05-14 19:58:56 -0400

    uncompressed data files, and switched endianness

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ SRC_FILES = $(wildcard src/*.cpp)
 BUILD_FILES = $(patsubst src/%.cpp, build/%.o, ${SRC_FILES})
 LIBS = 
 LFLAGS = $(shell pkg-config --libs ${LIBS})
-CFLAGS = -std=gnu++11 -g -O3 $(shell pkg-config --cflags ${LIBS})
+CFLAGS = -std=gnu++11 -g $(shell pkg-config --cflags ${LIBS})
 
 all: build ${BUILD_FILES}
 	g++ -o build/jellyVision ${BUILD_FILES} ${LFLAGS}
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,79 @@
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
+#include <algorithm>
+#include <vector>
+
 #include <Eigen/Dense>
 
 using namespace Eigen;
 using namespace std;
 
+//reverses endianness
+template <class T>
+void endswap(T *objp);
+
+
 int main(int argc, char **argv)
 {
-  
+
     VectorXd inputLayer(784);
     MatrixXd HiddenLayer(16, 784);
-    
-    ifstream imageFile ("train-images-idx3-ubyte.gz", ios::in | ios::binary);
+
+    ifstream imageFile ("train-images.idx3-ubyte", ios::in | ios::binary);
 
     if(imageFile.fail())
     {
-      cout << "ERROR: Image not found" << std::endl;
-      return 1;
+        cout << "ERROR: Image not found" << std::endl;
+        return 1;
     }
-    
-    char magic[4];
-    imageFile.read(magic, 4);
+
+    int32_t magic;
+    imageFile.read(reinterpret_cast<char *>(&magic), sizeof(magic));
+    endswap(&magic);
 
     cout << magic << std::endl;
-    
-    char imageCount[4];
-    imageFile.read(imageCount, 4);
+     
+    uint32_t imageCount;
+    imageFile.read(reinterpret_cast<char *>(&imageCount), sizeof(imageCount));
+    endswap(&imageCount);
 
-    char rowSize[4];
-    imageFile.read(rowSize, 4);
+    uint32_t rowSize;
+    imageFile.read(reinterpret_cast<char *>(&rowSize), sizeof(rowSize));
+    endswap(&rowSize);
 
-    char colSize[4];
-    imageFile.read(colSize, 4);
+    uint32_t colSize;
+    imageFile.read(reinterpret_cast<char *>(&colSize), sizeof(colSize));
+    endswap(&colSize);
 
     //loop through MRI file and seperate each pixel into its x,y,z cordinate
-    for (int row = 0; row < (int) *(rowSize); row++)
+    for (int row = 0; row < rowSize; row++)
     {
-      for (int col = 0; col < (int) *(colSize); col++)
+        for (int col = 0; col < colSize; col++)
         {
             //place data into a char buffer and then reinterpret into an unsigned char
             char pix[1];
             imageFile.read(pix, 1);
-	    inputLayer(col * row) = (double) reinterpret_cast<unsigned char&>(pix[0]) / 255;
+	    endswap(&pix);
+	    
+            inputLayer((row * rowSize) + col) = (double) reinterpret_cast<unsigned char&>(pix[0]) / 255;
         }
     }
 
     cout << "Matrix: " << inputLayer << std::endl;
-    
+
     return 0;
+}
+
+
+vector<VectorXd> readDigitImages()
+{
+}
+
+//reverses endianness
+template <class T>
+void endswap(T *objp)
+{
+    unsigned char *memp = reinterpret_cast<unsigned char*>(objp);
+    std::reverse(memp, memp + sizeof(T));
 }
